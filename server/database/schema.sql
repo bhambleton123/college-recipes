0d7e8abff30d864b8ee3322e609f26b015cdbf93
Brian Hambleton <bhambl3t0n1@gmail.com>
2019-11-16 13:20:57 -0800

    Started working on backend

---
+++ server/database/schema.sql
@@ -0,0 +1,26 @@
+DROP DATABASE IF EXISTS recipes;
+
+CREATE DATABASE recipes;
+
+USE recipes;
+
+CREATE TABLE users (
+  id int NOT NULL AUTO_INCREMENT,
+  user_name text,
+  password text,
+  salt text,
+  PRIMARY KEY(id)
+);
+
+CREATE TABLE recipes (
+  id int NOT NULL AUTO_INCREMENT,
+  title text,
+  user_id int REFERENCES users(id),
+  PRIMARY KEY(id)
+);
+
+CREATE TABLE recipe_step (
+  step_number SMALLINT,
+  step_description text,
+  recipe_id MEDIUMINT REFERENCES recipes(id)
+);
