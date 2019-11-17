4dcef8b9033b5105f60210f1cb142d356df53b0c
Brian Hambleton <bhambl3t0n1@gmail.com>
2019-11-16 23:54:56 -0800

    Verfied that verify token middleware is working

--- server/controllers/recipes.js
+++ server/controllers/recipes.js
@@ -1,51 +1,52 @@
-const models = require('../models/recipes.js');
+const models = require("../models/recipes.js");
+const utils = require('../utils.js');
 
 const getRecipes = (req, res) => {
   models.getRecipes((err, results) => {
-    if(err){
+    if (err) {
       res.status(500).send(err);
-    }
-    else{
-      res.send(results);
-    }
-  })
-}
-
-const postRecipe = (req, res) => {
-  models.postRecipe(req.params.user_id, req.body.title, (err, results) => {
-    if(err){
-      res.status(500).send(err);
-    }
-    else{
+    } else {
       res.send(results);
     }
   });
-}
+};
 
-const updateRecipeByUserId = (req, res) => {
-  models.updateRecipeByUserId(req.params.user_id, req.body.title, (err, results) => {
-    if(err){
+const postRecipe = (req, res) => {
+  models.postRecipe(req.params.user_id, req.body.title, (err, results) => {
+    if (err) {
       res.status(500).send(err);
-    }
-    else{
+    } else {
       res.send(results);
     }
-  })
-}
+  });
+};
+
+const updateRecipeByUserId = (req, res) => {
+  models.updateRecipeByUserId(
+    req.params.user_id,
+    req.body.title,
+    (err, results) => {
+      if (err) {
+        res.status(500).send(err);
+      } else {
+        res.send(results);
+      }
+    }
+  );
+};
 
 const deleteRecipeById = (req, res) => {
   models.deleteRecipeById(req.params.id, (err, results) => {
-    if(err){
+    if (err) {
       res.status(500).send(err);
-    }
-    else{
+    } else {
       res.send(results);
     }
-  })
-}
+  });
+};
 module.exports = {
   getRecipes,
   postRecipe,
   updateRecipeByUserId,
-  deleteRecipeById,
-}
+  deleteRecipeById
+};
